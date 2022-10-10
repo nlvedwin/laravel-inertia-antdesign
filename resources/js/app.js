3acab94cbe70b847a2d26734535bd888027baa3d
Edwin Encomienda <encomienda000@gmail.com>
2022-10-10 12:08:39 +0800

    updates

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,9 +7,6 @@ import { InertiaProgress } from '@inertiajs/progress';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-import AntDesign from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'; 
-import '../less/theme.less'
 import PersistentLayout from './Layouts/PersistentLayout.vue'
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
@@ -25,11 +22,6 @@ createInertiaApp({
     setup({ el, app, props, plugin }) {
         return createApp({ render: () => h(app, props) })
             .use(plugin)
-
-            // ant design components use
-            // .use(DatePicker)
-            .use({...AntDesign})
-
             .use(ZiggyVue, Ziggy)
             .mount(el);
     },
