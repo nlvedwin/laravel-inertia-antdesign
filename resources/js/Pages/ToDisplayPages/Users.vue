46c16e37ea4ba827f7d44ae50092984900416d62
ryoung11 <ryoung@netlinkvoice.com>
2022-12-14 08:14:52 +0800

    Added Search bar

--- resources/js/Pages/ToDisplayPages/Users.vue
+++ resources/js/Pages/ToDisplayPages/Users.vue
@@ -1,28 +1,65 @@
 <script setup>
-import Table from '@/Components/Table.vue';
-import { UserAddOutlined } from '@ant-design/icons-vue';
-import axios from 'axios';
-import {onMounted, ref } from 'vue';
-import AddNewUser from '../Forms/AddNewUser.vue';
-const props = defineProps({ selectedProject: String, users: Array })
+import Table from "@/Components/Table.vue";
+import { UserAddOutlined } from "@ant-design/icons-vue";
+import axios from "axios";
+import { ref } from "vue";
+import AddNewUser from "../Forms/AddNewUser.vue";
+const props = defineProps({ selectedProject: String, users: Array });
 
 const visible = ref(false);
 const toEditData = ref();
+const searchData = ref("");
 const getHide = (value) => {
     visible.value = value;
-}
+};
 const onEditRecord = (value) => {
-    axios.get('/api/users-management/' + value).then((response) => {
+    axios.get("/api/users-management/" + value).then((response) => {
         toEditData.value = response.data;
-        visible.value = true
-    })
-}
+        visible.value = true;
+    });
+};
+
+const onSearch = (search) => {
+    searchData.value = search;
+};
+
+const returnSearched = (value) => {
+    let newVal = [];
+    if (!searchData.value) {
+        return value;
+    } else {
+        newVal = value.filter((val) => {
+            return (
+                val.firstname.toLowerCase().includes(searchData.value) ||
+                val.lastname.toLowerCase().includes(searchData.value) ||
+                val.age.includes(searchData.value) ||
+                val.address.toLowerCase().includes(searchData.value)
+            );
+        });
+
+        return newVal;
+    }
+};
 </script>
 <template>
-    <div style="margin-bottom: 20px; float: right;">
-        <a-button @click="visible = !visible"><UserAddOutlined/> Add new User</a-button>
+    <a-space direction="vertical">
+        <a-input-search
+            placeholder="Search...."
+            style="width: 200px"
+            @search="onSearch"
+        />
+    </a-space>
+    <div style="margin-bottom: 20px; float: right">
+        <a-button @click="visible = !visible"
+            ><UserAddOutlined /> Add new User</a-button
+        >
     </div>
-    <Table :data="$page.props.users" @onEdit="onEditRecord"/>
+    <Table :data="returnSearched($page.props.users)" @onEdit="onEditRecord" />
 
-    <AddNewUser v-if="visible" :toEditData="toEditData" :show="visible || toEditData !== undefined" @hideModal="getHide($event)"></AddNewUser>
+    <AddNewUser
+        v-if="visible"
+        :toEditData="toEditData"
+        :show="visible || toEditData !== undefined"
+        @hideModal="getHide($event)"
+    ></AddNewUser>
 </template>
